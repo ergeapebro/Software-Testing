958e5227c2b9f849edd14c685905dfad251f1e17
ergeapebro <aldrin.pratama01@gmail.com>
2021-10-28 23:19:27 +0700

    Software-Testing
    Menggunakan library NUnit untuk melakukan unit testing

---
+++ File Tugas testing/TugasLibraryUnitTesting/TestUserValid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTesting
+{
+    public class TestUserValid
+    {
+        [TestFixture]
+        public class TugasLibraryTest
+        {
+            private User user;
+
+            [SetUp]
+            public void Init()
+            {
+                user = new User();
+            }
+
+            [Test]
+            public void TestUserValid()
+            {
+                Assert.AreEqual(true, user.IsValidUser("admin", "admin"));
+            }
+        }
+    }
+}
